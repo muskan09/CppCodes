01f3e4ec20b20e2bc26c673da1b96ddea4566e54
muskan09 <muskankalsi9@gmail.com>
2018-11-15 21:49:09 +0530

    cpp codes

---
+++ cpp3/cppext.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+class mat(){
+public:
+int n;
+int m[10][10];
+void getdata();
+mat sum+();
+mat diff-();
+};
+mat::void getdata(){
+for(int i=0;i<2;i++){
+for(int j=0;j<2;j++){
+cin>>m[i][j];
+}
+}
+}
+mat::mat sum+(){
+mat o1,o2
+for(int i=0;i<2;i++){
+for(int j=0;j<2;j++){
+
+}
+}
+
+}
+
+int main(){
+mat m1,m2,m3;
+
+}
