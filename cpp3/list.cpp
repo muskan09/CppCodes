9bc000b98fd7f61bcf4736c530cdbabe0edabfb0
muskan09 <muskankalsi9@gmail.com>
2018-11-15 21:50:56 +0530

    cpp codes

---
+++ cpp3/list.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<list>
+using namespace std;
+int main(){
+int n,x;
+list<int>l;
+list<int>::iterator t;
+cin>>n;
+int y;
+list<int>l1;
+for(i=0;i<n;i++){
+cin>>x;
+y=x*2;
+l.pushback(x);
+l1.pushback(y);
+}
+
+return 0;
+}
