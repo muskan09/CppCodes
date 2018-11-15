8e584d9572fb5c2e929624ff6d1ba887d1671f6b
muskan09 <muskankalsi9@gmail.com>
2018-11-15 21:55:45 +0530

    cpp codes

---
+++ cpp3/tempsbs.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+int i,j,n;
+string arr[20],temp;
+cout<<"size";
+cin>>n;
+for(i=0;i<n;i++){
+cin>>arr[i];
+}
+for(int i=0;i<n;i++){
+for(int j=0;j<(n-i-1);j++){
+if(arr[j]>arr[j+1])
+{
+temp=arr[j];
+arr[j]=arr[j+1];
+arr[j+1]=temp;
+}
+}
+}
+cout<<"output";
+for(i=0;i<n;i++){
+cout<<arr[i];
+}
+return 0;
+}
